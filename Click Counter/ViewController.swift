04d85023f002ac84f6bd705e1e466e8e94b48af5
richard <richlip@googlemail.com>
2021-01-09 12:05:27 +0100

    Initial Commit

---
+++ Click Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Richard Lipski on 09.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
